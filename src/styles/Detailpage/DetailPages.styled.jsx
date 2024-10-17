a30aee7b2b3a7f0522b1d8dc4a8684c0f351d8de
sieun1002 <nata20034@naver.com>
2024-10-17 12:05:29 +0900

    [1017] sieun 한국야구 간격 조절

--- src/styles/Detailpage/DetailPages.styled.jsx
+++ src/styles/Detailpage/DetailPages.styled.jsx
@@ -219,7 +219,7 @@ export const ChattingInput = styled.input`
 export const DotsLoader = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
+  /* justify-content: center; */
   width: 100px;
   height: 20px;
 `;
@@ -369,6 +369,7 @@ export const NoticeDiv = styled.div`
 `;
 
 export const OneBitImg = styled.img`
+  margin-top: 15px;
   width: 100%;
   height: auto;
 `;
