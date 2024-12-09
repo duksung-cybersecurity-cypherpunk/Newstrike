0cd4f466c971d938642594d535f1a61bd9de75fa
sieun1002 <nata20034@naver.com>
2024-12-09 17:08:43 +0900

    Add files to frontend branch

--- src/api/api.js
+++ src/api/api.js
@@ -2,15 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   //원래 url
-  // baseURL: 'https://humble-commonly-goshawk.ngrok-free.app/',
-
-  // 임시 url 다운
-  // baseURL: "https://catfish-solid-specially.ngrok-free.app/",
-  // imgBaseURL: "https://catfish-solid-specially.ngrok-free.app",
-
-  // 임시 url 2 나영
-  baseURL: "https://humble-commonly-goshawk.ngrok-free.app/",
-  imgBaseURL: "https://humble-commonly-goshawk.ngrok-free.app",
+  baseURL: "baseURL 주소",
+  imgBaseURL: "imgBaseURL 주소",
 });
 
 export default api;
